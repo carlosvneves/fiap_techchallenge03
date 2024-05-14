a0d872b72383f8eb26a9693a05a056b5bc75b2de
CARLOS EDUARDO NEVES <carlos.vneves@gmail.com>
2024-05-14 00:01:02 -0300

    versão concluida

--- pages/4 Conclusão.py
+++ pages/4 Conclusão.py
@@ -3,3 +3,35 @@ import pandas as pd
 
 
 st.title('CONCLUSÃO')
+
+
+st.write('''
+         Os dados analisados correspondem ao período de início da pandemia, o qual é interessante para avaliar os efeitos das reações das pessoas, empresas e do Poder Público frente 
+         ao desafio imposto pela Covid.
+
+         É sempre bom lembrar que a análise exposta neste painel é dependente dos dados coletados que, dentre outros aspectos está limitada pela metodologia de coleta e sua qualidade.
+
+        # Principais observações:
+         -  O número de pessoas pesquisada aumentou entre maio e julho de 2020;
+         -  A maior parte dos entrevistados se encontra nos estados de Minas Gerais e São Paulo;
+         -  A maior parte dos entrevitados reside em área urbana;
+         -  A proporção entre homens e mulheres é aproximadamente a mesma;
+         -  A pesquisa foi feita considerando majoritamente a população branca e parda;
+         -  A maior parte dos entrevistados possui entre 19 e 44 anos;
+         -  Prevalece entre os entrevistados a baixa escolaridade;
+         -  Em que pese o avanço da pandemia, o número de pessoas trabalhando na semana anterior à entrevista aumentou ao longo do tempo, ao passo queo número de afastados reduziu. Inclusive
+         a quarentena como motivo de afastamento foi perdendo força ao longo do tempo; 
+         -  A proporção de pessoas que não trabalhavam em qualquer esfera de governo foi sempre maior;
+         -  De maio a julho de 2020, apenas os sintomas relacionados a perda de olfato, paladar, diarreia e dores musculares na semana anterior 
+         tiveram uma tendência de alta a partir do mês de junho de 2020. Isto pode ajudar a explicar o porquê de as pessoas estarem menos propensas a 
+         se afastarem do seu trabalho (claro que há vários outros fatores de cunho econômico);
+         -  O índice de testagem e detecção durante o período analisado foi relativamente baixo.
+
+         
+        # Principais ações:         
+        - O Hospital, em parceria com governos e representantes da sociedade civil organizada devem estar preparados para campanhas mais lúdicas e esclarecedoras acerca
+         da necessidade de prevenção e testagem em face de um novo surto, haja vista a baixa escolaridade da população em geral;
+        - O Hospital deve estar preparado para medidas de contigência em caso de "ondas" muito fortes de contaminação;
+        - Investimento no atendimento precoce, de modo que os sintomas sejam mais leves, auxilia a recuperação mais rápida e o retorno ao mercado de trabalho, o que facilita também as medidas de prevenção na propagação da doença;
+        
+''')
